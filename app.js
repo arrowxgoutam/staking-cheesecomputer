909fc01f97eb342ef4a1e772e086684e3f25a40e
“arrowxgoutam” <goutampaik42@gmail.com>
2023-03-06 00:09:47 +0600

    for Client

--- app.js
+++
@@ -1,6 +0,0 @@
-const dotElement = document.querySelector('.dot');
-
-setInterval(() => {
-  dotElement.classList.toggle('online');
-  dotElement.classList.toggle('offline');
-}, 1000);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="cheesecomputer">
+         <iframe src="https://cheese-computer.vercel.app/cheesecomputer" frameborder="0"></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default App;
